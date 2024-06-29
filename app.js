a21c7c39924b71cfa1aac4f128d1fede77b7192f
Yasin <eliteyasin1@gmail.com>
2024-06-29 12:45:19 +0500

    cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json())
 // cors
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ecommerceclient-15f2.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
