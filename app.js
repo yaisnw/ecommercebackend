2ef1fc59811d68d06c048318908070b8683534ec
Yasin <eliteyasin1@gmail.com>
2025-05-31 17:23:05 +0500

    cors origin

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 const cors = require('cors');
 
 app.use(cors({
-  origin: '*',
+  origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
